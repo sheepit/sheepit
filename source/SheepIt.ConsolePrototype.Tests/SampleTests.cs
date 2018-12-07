063669eb14409077a466435439d50272980bcbdb
redss <ryszard.tarajkowski@gmail.com>
2018-12-07 17:15:57 +0100

    Added tests project.

---
+++ source/SheepIt.ConsolePrototype.Tests/SampleTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace SheepIt.ConsolePrototype.Tests
+{
+    public class SampleTests
+    {
+        [Test]
+        public void nunit_works()
+        {
+        }
+    }
+}
