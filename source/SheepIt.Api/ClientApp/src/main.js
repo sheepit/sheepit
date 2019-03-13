0635b40505110335025324ecf59a252856cd6706
Tomasz Spendel <tomaszspendel@gmail.com>
2019-03-13 15:56:47 +0100

    #91 Attached bootstrap

--- source/SheepIt.Api/ClientApp/src/main.js
+++ source/SheepIt.Api/ClientApp/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
