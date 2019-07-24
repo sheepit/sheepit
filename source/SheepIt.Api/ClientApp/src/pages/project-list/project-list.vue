87190db807f22dad6413f58682004b93e319bf5d
Tomasz Spendel <tomaszspendel@gmail.com>
2019-07-24 22:05:36 +0200

    #132 Restored create project button

--- source/SheepIt.Api/ClientApp/src/pages/project-list/project-list.vue
+++ source/SheepIt.Api/ClientApp/src/pages/project-list/project-list.vue
@@ -1,45 +1,58 @@
 <template>
-    <expanding-list
-        v-if="projects && projects.length > 0"
-        class="mt-4"
-        :all-items="projects"
-        initial-length="10000">
-        <template slot-scope="{ items }">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th scope="col">
-                            Project ID
-                        </th>
-                        <th scope="col">
-                            Repository url
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr
-                        v-for="item in items"
-                        :key="item.id">
-                        
-                        <td scope="row">
-                            <router-link
-                                :to="{ name: 'project', params: { projectId: item.id }}">
-                                {{ item.id }}
-                            </router-link>
-                        </td>
-                        <td>
-                            {{ item.repositoryUrl }}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </template>
-    </expanding-list>
-    <div v-else-if="projects && projects.length > 0">
-        There are no projects yet
-    </div>
-    <div v-else>
-        Loading...
+    <div>
+        <h2>Projects</h2>
+        <div class="row">
+            <div class="col text-right">
+                <router-link
+                    class="btn btn-primary"
+                    :to="{ name: 'create-project' }"
+                >
+                    Create project
+                </router-link>
+            </div>
+        </div>
+        <expanding-list
+            v-if="projects && projects.length > 0"
+            class="mt-4"
+            :all-items="projects"
+            initial-length="10000">
+            <template slot-scope="{ items }">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">
+                                Project ID
+                            </th>
+                            <th scope="col">
+                                Repository url
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr
+                            v-for="item in items"
+                            :key="item.id">
+                            
+                            <td scope="row">
+                                <router-link
+                                    :to="{ name: 'project', params: { projectId: item.id }}">
+                                    {{ item.id }}
+                                </router-link>
+                            </td>
+                            <td>
+                                {{ item.repositoryUrl }}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </template>
+        </expanding-list>
+        <div v-else-if="projects && projects.length > 0">
+            There are no projects yet
+        </div>
+        <div v-else>
+            Loading...
+        </div>
     </div>
 </template>
 
