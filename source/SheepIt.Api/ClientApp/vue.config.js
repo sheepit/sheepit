7989c4615d9dcdc5a951e6544119ee39b58cd712
Tomasz Spendel <tomaszspendel@gmail.com>
2019-07-25 15:21:25 +0200

    Fixed yarn hot reload

--- source/SheepIt.Api/ClientApp/vue.config.js
+++ source/SheepIt.Api/ClientApp/vue.config.js
@@ -3,6 +3,9 @@ const path = require("path");
 module.exports = {
     outputDir: path.resolve(__dirname, "../wwwroot"),
     devServer: {
-        port: 8088
+        port: 8089,
+        watchOptions: {
+            poll: true
+        }
     }
 };
