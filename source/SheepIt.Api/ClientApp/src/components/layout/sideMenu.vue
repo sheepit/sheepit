457a3559506850bea93cbdec636c6a7db73b1411
Tomasz Spendel <tomaszspendel@gmail.com>
2020-02-20 19:13:24 +0100

    #239 Converted sidemenu to separate component. Changed layout grid.

---
+++ source/SheepIt.Api/ClientApp/src/components/layout/sideMenu.vue
@@ -0,0 +1,101 @@
+<template>
+    <div>
+        <div class="sidemenu_placeholder"></div>
+
+        <ul class="sidemenu">
+            <li class="sidemenu__item">
+                <a class="sidemenu__link" href="#">
+                    <div class="sidemenu__link--icon">
+                        <font-awesome-icon icon="code" />
+                    </div>
+                    <div class="sidemenu__link--name">
+                        Components
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a class="sidemenu__link" href="#">
+                    <div class="sidemenu__link--icon">
+                        <font-awesome-icon icon="server" />
+                    </div>
+                    <div class="sidemenu__link--name">
+                        Environments
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a class="sidemenu__link" href="#">
+                    <div class="sidemenu__link--icon">
+                        <font-awesome-icon icon="cubes" />
+                    </div>
+                    <div class="sidemenu__link--name">
+                        Packages
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a class="sidemenu__link" href="#">
+                    <div class="sidemenu__link--icon">
+                        <font-awesome-icon icon="cogs" />
+                    </div>
+                    <div class="sidemenu__link--name">
+                        Deployments
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SideMenu"
+}   
+</script>
+
+<style lang="scss" scoped>
+.sidemenu_placeholder {
+    height: 30px;
+}
+
+.sidemenu {
+    padding-left: 0px;
+    list-style: none;
+
+    color: #898989;
+    font-size: 16px;
+
+    &__item {
+    }
+
+    &__link {
+        display: flex;
+        align-items: center;
+        padding: 12px;
+        line-height: 21px;
+        color: #898989;
+        text-decoration: none;
+
+        &--icon {
+            display: flex;
+            align-items: center;
+            margin-left: 9px;
+            margin-right: 9px;
+
+            svg {
+                height: 16px;
+                width: 16px;
+            }
+        }
+
+        &--name {
+            flex: 1;
+        }
+
+        &:hover {
+            background: #BABABA;
+            color: #FFFFFF;
+        }
+    }
+}
+</style>
--- source/SheepIt.Api/ClientApp/src/pages/default-layout/default-layout.vue
+++ source/SheepIt.Api/ClientApp/src/pages/default-layout/default-layout.vue
@@ -9,73 +9,23 @@
             </header>
 
             <nav class="ddd">
-                <div class="sidemenu_placeholder"></div>
-
-                <ul class="sidemenu">
-                    <li class="sidemenu__item">
-                        <a class="sidemenu__link" href="#">
-                            <div class="sidemenu__link--icon">
-                                <font-awesome-icon icon="code" />
-                            </div>
-                            <div class="sidemenu__link--name">
-                                Components
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="sidemenu__link" href="#">
-                            <div class="sidemenu__link--icon">
-                                <font-awesome-icon icon="server" />
-                            </div>
-                            <div class="sidemenu__link--name">
-                                Environments
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="sidemenu__link" href="#">
-                            <div class="sidemenu__link--icon">
-                                <font-awesome-icon icon="cubes" />
-                            </div>
-                            <div class="sidemenu__link--name">
-                                Packages
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="sidemenu__link" href="#">
-                            <div class="sidemenu__link--icon">
-                                <font-awesome-icon icon="cogs" />
-                            </div>
-                            <div class="sidemenu__link--name">
-                                Deployments
-                            </div>
-                        </a>
-                    </li>
-                </ul>
+                <side-menu />
             </nav>
 
             <main>
                 <div class="ddd2">
-                    CONTENT
+                    <router-view />
                 </div>
             </main>
         </div>
 
-        <top-bar />
-
         <breadcrumb />
-
-        <div class="container">
-            <main class="mt-4">
-                <router-view />
-            </main>
-        </div>
     </div>
 </template>
 
 <script>
 import TopBar from '../../components/layout/topBar.vue'
+import SideMenu from '../../components/layout/sideMenu.vue'
 import Breadcrumb from "../../components/breadcrumb.vue";
 
 export default {
@@ -83,6 +33,7 @@ export default {
 
     components: {
         TopBar,
+        SideMenu,
         Breadcrumb
     },
 
@@ -99,11 +50,11 @@ export default {
     display: grid;
 
     grid-template-areas:
-    "header header header"
-    "nav content .";
+    "header header"
+    "nav content";
 
-    grid-template-columns: 220px 1fr 220px;
-    grid-template-rows: auto 1fr auto;
+    grid-template-columns: 220px 1fr;
+    grid-template-rows: auto 1fr;
     grid-column-gap: 10px;
 
     height: 100vh;
@@ -111,7 +62,7 @@ export default {
 
 .ddd {
     border-style: solid;
-    border-width: 0 1px 0 1px;
+    border-width: 0 1px 0 0;
     border-color: #EBEBEB;
 
     height: 100%;
@@ -148,50 +99,5 @@ main {
     grid-area: content;
 
     justify-self: center;
-}
-
-.sidemenu_placeholder {
-    height: 30px;
-}
-
-.sidemenu {
-    padding-left: 0px;
-    list-style: none;
-
-    color: #898989;
-    font-size: 16px;
-
-    &__item {
-    }
-
-    &__link {
-        display: flex;
-        align-items: center;
-        margin: 0 1px 0 4px;
-        padding: 12px;
-        line-height: 21px;
-        color: #898989;
-        text-decoration: none;
-
-        &--icon {
-            display: flex;
-            align-items: center;
-            margin-right: 9px;
-
-            svg {
-                height: 16px;
-                width: 16px;
-            }
-        }
-
-        &--name {
-            flex: 1;
-        }
-
-        &:hover {
-            background: #BABABA;
-            color: #FFFFFF;
-        }
-    }
 }
 </style>
