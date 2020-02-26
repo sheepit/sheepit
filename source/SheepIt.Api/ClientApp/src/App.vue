b0408f2bcd0b3b04caad1b1cdca2527384652568
Tomasz Spendel <tomaszspendel@gmail.com>
2020-02-26 20:55:03 +0100

    Updated font color

--- source/SheepIt.Api/ClientApp/src/App.vue
+++ source/SheepIt.Api/ClientApp/src/App.vue
@@ -205,12 +205,3 @@ export default {
     router
 };
 </script>
-
-<style>
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-}
-</style>
