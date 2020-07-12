db7b9a60018874c3b14061027bd58b590fc5235f
redss <ryszard.tarajkowski@gmail.com>
2020-07-12 23:45:29 +0200

    #318 Fixed variables not being set.

--- source/SheepIt.Api/ClientApp/src/pages/package/create/_services/create-package-service.js
+++ source/SheepIt.Api/ClientApp/src/pages/package/create/_services/create-package-service.js
@@ -14,8 +14,11 @@ export default {
         formData.append('ComponentId', componentId);
         formData.append('Description', description);
         formData.append('ZipFile', zipFileData);
+        
+        // we need to unwrap object from Vue's observables in order to process it
+        const unwrappedNewVariables = JSON.parse(JSON.stringify(newVariables));
 
-        this.convertVariablesToFormData(formData, newVariables, environments);
+        this.convertVariablesToFormData(formData, unwrappedNewVariables, environments);
 
         return httpService
             .postFormData(
@@ -24,34 +27,32 @@ export default {
             );
     },
 
-    convertVariablesToFormData(formData, newVariables, environments) {
+    convertVariablesToFormData(formData, newVariables) {
         if (!newVariables) {
             return;
         }
+        
+        newVariables.forEach((variablesRow, variablesRowIndex) => {
 
-        for(let variablesIndex = 0; variablesIndex < newVariables.length; variablesIndex++) {
-            const variables = newVariables[variablesIndex];
-            
-            formData.append('VariableUpdates[' + variablesIndex + '].Name', variables.name);
-            
-            if (variables.defaultValue) {
-                formData.append('VariableUpdates[' + variablesIndex + '].DefaultValue', variables.defaultValue);
+            formData.append(`VariableUpdates[${variablesRowIndex}].Name`, variablesRow.name);
+
+            if (variablesRow.defaultValue) {
+                formData.append(`VariableUpdates[${variablesRowIndex}].DefaultValue`, variablesRow.defaultValue);
             }
 
-            this.mapEnvironmentValuesToFormData(
-                formData, variablesIndex, variables.environmentValues, environments);
-        }
-    },
-
-    mapEnvironmentValuesToFormData(formData, index, values, environments) {
-        for(let eindex = 0; eindex < environments.length; eindex++) {
-            let environmentId = environments[eindex].id;
-
-            let valueForEnvironment = values[environmentId];
-            if(valueForEnvironment) {
-                formData.append('VariableUpdates[' + index + '].EnvironmentValues[' + eindex + '].Key', environmentId);
-                formData.append('VariableUpdates[' + index + '].EnvironmentValues[' + eindex + '].Value', valueForEnvironment);
-            }
-        }
+            Object.entries(variablesRow.environmentValues)
+                .map(([environmentId, value]) => ({
+                    environmentId,
+                    value
+                }))
+                .filter(variable => variable.value)
+                .forEach((variable, variableIndex) => {
+                    formData.append(`VariableUpdates[${variablesRowIndex}].EnvironmentValues[${variableIndex}].Key`, variable.environmentId);
+                    formData.append(`VariableUpdates[${variablesRowIndex}].EnvironmentValues[${variableIndex}].Value`, variable.value);
+                });
+            
+        });
+        
     }
+
 }
